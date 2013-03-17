e4a68af97d13d50cf63c51e1d884b2d0e6fa5307
bretjg <bret.glassett@gmail.com>
2013-03-17 09:22:35 -0700

    added some modules

---
+++ sites/all/themes/aup/js/scripts.js
@@ -0,0 +1,159 @@
+$(document).ready(function() {
+	 var temp =  $(".main .siteSearch").html();
+	 $(".mobileHeader .siteSearch").html(temp);
+	 //temp =  $(".main .topic").html();
+	 //$(".mobileHeader .topic").html(temp);
+	 temp =  $(".audience").html();
+	 $(".mobileHeader .siteSearch").html(temp);
+	 temp =  $(".secondaryNav").html();
+	 $(".mobileHeader .infomenulist").html(temp);
+	 
+	 
+	 
+});
+
+$(document).ready(function() {
+	function setInputAction(cssClass, val){
+  		$(cssClass).focus(function() {if(this.value == val){this.value =""}});
+  		$(cssClass).blur(function() {if(this.value == ""){this.value = val}});
+	}
+	setInputAction('.txtSiteSearch', 'Search');
+});
+
+$(document).ready(function() {
+	var selected = 0;
+	var anim = 600;
+
+	$(".mobileHeader .menu a").click(function(){
+		if(selected == 1){
+			$(".mobileHeader .topic").slideUp(anim, 0);
+			selected = 0;
+		}
+		else{
+			$(".mobileHeader .infomenulist").slideUp(anim, 0);
+			$(".mobileHeader .siteSearch").slideUp(anim, 0);
+			$(".mobileHeader .topic").slideDown(anim, 0);
+			selected = 1;
+		}
+		return false;
+	});
+	
+	$(".mobileHeader .infomenu a").click(function(){
+		if(selected == 2){
+			$(".mobileHeader .infomenulist").slideUp(anim, 0);
+			selected = 0;
+		}
+		else{
+			$(".mobileHeader .topic").slideUp(anim, 0);
+			$(".mobileHeader .siteSearch").slideUp(anim, 0);
+			$(".mobileHeader .infomenulist").slideDown(anim, 0);
+			selected = 2;
+		}
+		return false;
+	});
+	
+	$(".mobileHeader .search a").click(function(){
+		if(selected == 3){
+			$(".mobileHeader .siteSearch").slideUp(anim, 0);
+			selected = 0;
+		}
+		else{
+			$(".mobileHeader .infomenulist").slideUp(anim, 0);
+			$(".mobileHeader .topic").slideUp(anim, 0);
+			$(".mobileHeader .siteSearch").slideDown(anim, 0);
+			selected = 3;
+		}
+		return false;
+	});
+	
+	var mobileSearchDisplay = false;
+	$(".mobileSearchButton a").click(function(){
+		if(mobileSearchDisplay){
+			$(this).removeClass("btnOn");
+			mobileSearchDisplay = false;
+			$(".mobileSiteSearch").slideUp(300);	
+		}
+		else{
+			$(this).addClass("btnOn");
+			mobileSearchDisplay = true;	
+			$(".mobileSiteSearch").slideDown(300);
+		}
+		
+	});
+	
+});
+
+
+$(document).ready(function() {
+	var gcount = 1;
+	$(".gTabs").children().each(function() {
+		addGatewayClickEvent($(this), gcount);
+		gcount++;
+   	 });
+	 function addGatewayClickEvent(listitem, counter){
+		 $(listitem.children()).click(function() {toggleGateway(counter); return false;});
+	 }
+	function toggleGateway(id){
+		$(".gTabs li").removeClass("selected");
+		$(".gInner").removeClass("selected");
+		var contentClass = ".gInner" + id;
+		var menuClass = ".gTabs .tab-" + id;
+		$(contentClass).addClass("selected");
+		$(menuClass).addClass("selected");	
+	}
+	if ($(".gatewayFeature").html() != null){
+		var gatewaySections = $('.gatewayFeature .gTabs').children().length;
+		//alert(gatewaySections);
+		if(gatewaySections > 0){
+			var sb = "";
+			for(var i = 1; i <= gatewaySections; i++){
+				sb += '<div class="accordion accordion' + i + '">';
+				sb += '<div class="accTitle"><a href="#" rel="' + i + '">' + $('.gTabs .tab-' + i + ' a').html() + '</a></div>';
+				sb += '<div class="accContent">' + $('.gContent .gInner' + i).html() + '</div>';
+				sb += '</div>';
+			}
+			$('.gatewayFeature').after('<div class="mobileGatewayFeature">' + sb + '</div>');
+			$('.accordion1 .accContent').show();
+			$('.accordion1 .accContent').addClass("open");
+			$('.accordion .accTitle a').click(function() { accordionSwitch($(this).attr("rel"));return false;});
+		}
+	}
+	
+	
+	if ($(".accordionPod").html() != null){
+		$('.accordionPod .accTitle a').click(function() { accordionSwitch($(this).attr("rel"));return false;});
+	}
+	function accordionSwitch(id){
+		if($('.accordion' + id + ' .open').html() == null){
+			$('.accordion .open').removeClass("open");
+			$('.accordion .tabOpen').removeClass("tabOpen");
+			$('.accordion' + id + ' .accContent').addClass("open");
+			$('.accordion' + id + ' .accTitle').addClass("tabOpen");
+			$('.accordion .accContent').slideUp(300);
+			$('.accordion' + id + ' .accContent').slideDown(300);
+		}
+	}
+	
+});
+
+
+
+/** social pod tabs **/
+$(document).ready(function() {
+	var gcount = 1;
+	$(".podSocial .gTabs").children().each(function() {
+		addSocialEvent($(this), gcount);
+		gcount++;
+   	 });
+	 function addSocialEvent(listitem, counter){
+		 $(listitem.children()).click(function() {toggleSocial(counter); return false;});
+	 }
+	function toggleSocial(id){
+		$(".podSocial .gTabs li").removeClass("selected");
+		$(".podSocial .gInner").removeClass("selected");
+		var contentClass = ".podSocial .gInner" + id;
+		var menuClass = ".podSocial .gTabs .tab-" + id;
+		$(contentClass).addClass("selected");
+		$(menuClass).addClass("selected");	
+	}
+});
---
+++ sites/all/themes/aup/page.tpl.php
@@ -0,0 +1,262 @@
+<div class="outer">
+        	<div class="outerBg">
+            	<div class="header homeHeader">
+                    <div class="audience">
+                        <ul>
+                            <li><a href="#">Prospective students</a></li>
+                            <li><a href="#">Current Students</a></li>
+                            <li><a href="#">Faculty &amp; Staff</a></li>
+                            <li><a href="#">Parents &amp; Families</a></li>
+                            <li><a href="#">Alumni</a></li>
+                            <li><a href="#">Employers</a></li>
+                        </ul>
+                    </div>
+                    <div class="title"></div>
+                    <div class="main">
+                    	<div class="mobileShow">
+                            <div class="mobileSiteSearch">
+                                <form method="post" action="#">
+                                    <fieldset>
+                                        <legend>Search</legend>
+                                        <label for="txtSiteSearch">Search</label>
+                                        <input type="text" class="txtSiteSearch" id="txtSiteSearch" value="Search" />
+                                        <input type="submit" class="btnSiteSubmit" value="" />
+                                    </fieldset>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="logo">
+                            <a href="#"><img src="images/logo.png" alt="" /></a>
+                            <div class="mobileShow mobileSearchButton"><a href="#"><span>Search</span></a></div>
+                        </div>
+                    	<div class="topic">
+                        	<ul>
+                            	<li><a href="#">About</a></li>
+                            	<li><a href="#">Academics</a></li>
+                            	<li><a href="#">Admissions</a></li>
+                            	<li><a href="#">Student Life</a></li>
+                            	<li><a href="#">News &amp; Events</a></li>
+                            	<li><a href="#">Support AUP</a></li>
+                            </ul>
+                        </div>
+                        <div class="utility">
+                        	<ul>
+                            	<li><a href="#">Contact Us</a></li>
+                                <li class="last"><a href="#">Visit AUP</a></li>
+                            </ul>
+                            <div class="siteSearch">
+                            	<form method="post" action="#">
+                                	<fieldset>
+                                     	<legend>Search</legend>
+                                        <label for="txtSiteSearch">Search</label>
+                                        <input type="text" class="txtSiteSearch" id="txtSiteSearch" value="Search" />
+                                        <input type="submit" class="btnSiteSubmit" value="" />
+                                    </fieldset>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mobileShow mobileHeader">
+                        <div class="tabs">
+                        	<div class="menu">
+                            	<a href="#">Menu</a>
+                            </div>
+                        	<div class="search">
+                            	<a href="#">Info for</a>
+                            </div>
+                        </div>
+                        <div class="ddContainer">
+                        	<div class="siteSearch">
+                            
+                            </div>
+                            <div class="topic">
+                                <ul>
+                                    <li><a href="#">About</a></li>
+                                    <li><a href="#">Academics</a></li>
+                                    <li><a href="#">Admissions</a></li>
+                                    <li><a href="#">Student Life</a></li>
+                                    <li><a href="#">News &amp; Events</a></li>
+                                    <li><a href="#">Support AUP</a></li>
+                                    <li><a href="#">Contact Us</a></li>
+                                    <li><a href="#">Visit AUP</a></li>
+                                </ul>
+                            </div>
+                            <div class="infomenulist">
+                            	
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="homeCarousel">
+                    <ul class="bxslider">
+                        <li>
+                            <img src="images/homeBanner.jpg" alt="" />
+                            <div class="caption">
+                            	<div class="text mobileHide"><img src="images/homeBannerCaption.png" alt="" /></div>
+                            	<div class="text mobileShow"><span>At</span> Home <em>in</em> <strong>the world</strong></div>
+                                <h2>Your classroom is Paris, A city that has made being yourself an art form</h2>
+                                <div class="more"><a href="#">Find your Paris</a></div>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="images/homeBanner.jpg" alt="" />
+                            <div class="caption">
+                                <h2>Slide 2</h2>
+                                <div class="more"><a href="#">Learn more</a></div>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="images/homeBanner.jpg" alt="" />
+                            <div class="caption">
+                                <h2>Slide 3</h2>
+                                <div class="more"><a href="#">Learn more</a></div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <!-- Start of wrapper -->
+                <div class="wrapper">
+                    <!-- Start of Content -->
+                    <div class="content homeContent">
+						<div class="fullWidth promos">
+                        	<ul class="feed">
+                                <li>
+                                    <h3>Students <em>of</em> the World</h3>
+                                    <p><a href="#"><img src="images/promo1.jpg" alt="" /></a></p>
+                                    <h4><a href="#">A Variety of Fellow Students</a></h4>
+                                    <p>The training I received in and out of the classroom has been useful in countless ways...</p>
+                                </li>
+                                <li class="wideCol">
+                                    <h3>Students <em>of</em> the World</h3>
+                                    <p><a href="#"><img src="images/promo2.jpg" alt="" /></a></p>
+                                    <h4><a href="#">A Variety of Fellow Students</a></h4>
+                                    <p>The training I received in and out of the classroom has been useful in countless ways...</p>
+                                </li>
+                                <li>
+                                    <h3>Students <em>of</em> the World</h3>
+                                    <p><a href="#"><img src="images/promo3.jpg" alt="" /></a></p>
+                                    <h4><a href="#">A Variety of Fellow Students</a></h4>
+                                    <p>The training I received in and out of the classroom has been useful in countless ways...</p>
+                                </li>
+                            </ul>
+                        </div>
+						<div class="fullWidth news">
+                        	<div class="feedTitle">
+                            	<h3>News <a href="#" class="rss"><img src="images/rss.png" alt=""/></a></h3>
+                                <div class="more">
+                                	<a href="#">View more</a>
+                                </div>
+                            </div>
+                            <ul class="feed">
+                                <li>
+                                    <h4><a href="#">A Variety of Fellow Students</a> <span>October 25</span></h4>
+                                    <p>Join Celeste Schenck, President of The American University of Paris, for a thought-provoking discussion about the increasing fit between women's...</p>
+                                </li>
+                                <li class="wideCol">
+                                    <h4><a href="#">A Variety of Fellow Students</a> <span>October 25</span></h4>
+                                    <p>Join Celeste Schenck, President of The American University of Paris, for a thought-provoking discussion about the increasing fit between women's...</p>
+                                </li>
+                                <li>
+                                    <h4><a href="#">A Variety of Fellow Students</a> <span>October 25</span></h4>
+                                    <p>Join Celeste Schenck, President of The American University of Paris, for a thought-provoking discussion about the increasing fit between women's...</p>
+                                </li>
+                            </ul>
+                        </div>
+						<div class="fullWidth events">
+                        	<div class="feedTitle">
+                            	<h3>Events <a href="#" class="rss"><img src="images/rss.png" alt=""/></a></h3>
+                                <div class="more">
+                                	<a href="#">View more</a>
+                                </div>
+                            </div>
+                            <ul class="feed">
+                                <li>
+                                	<div class="date">
+                                    	<span>October</span>
+                                        <em>22</em>
+                                    </div>
+                                    <h4><a href="#">Women's Leadership for the 21st Century</a> <span>6.30 - 8.30 PM</span></h4>
+                                    <p>Join Celeste Schenck, President of The American University of Paris, for a thought-provoking discussion about the increasing fit between women's...</p>
+                                </li>
+                                <li class="wideCol">
+                                	<div class="date">
+                                    	<span>October</span>
+                                        <em>22</em>
+                                    </div>
+                                    <h4><a href="#">Women's Leadership for the 21st Century</a> <span>6.30 - 8.30 PM</span></h4>
+                                    <p>Join Celeste Schenck, President of The American University of Paris, for a thought-provoking discussion about the increasing fit between women's...</p>
+                                </li>
+                                <li>
+                                	<div class="date">
+                                    	<span>October</span>
+                                        <em>22</em>
+                                    </div>
+                                    <h4><a href="#">Women's Leadership for the 21st Century</a> <span>6.30 - 8.30 PM</span></h4>
+                                    <p>Join Celeste Schenck, President of The American University of Paris, for a thought-provoking discussion about the increasing fit between women's...</p>
+                                </li>
+                            </ul>
+                        </div>
+						<div class="fullWidth social">
+                            <ul class="feed">
+                                <li>
+                                     <h5>
+                                        <img src="images/facebook32.png" alt="" />
+                                        <span>Facebook</span>
+                                        <a href="#">View more</a>
+                                     </h5>
+                                     <p>Have you seen the website dedicated to AUP's 50th anniversary? Check it out! http://50years.aup.edu/ There are two 50th anniversary celebration events in November: Alumni Event in San Francisco- 1 November, 2012 Alumni... <a href="#">Read More</a></p>
+                                </li>
+                                <li class="wideCol">
+                                     <h5>
+                                        <img src="images/youtube32.png" alt="" />
+                                        <span>youtube</span>
+                                        <a href="#">View more</a>
+                                     </h5>
+                                     <iframe width="100%" height="180" src="http://www.youtube-nocookie.com/embed/jBFilmmihcc?rel=0" frameborder="0" allowfullscreen></iframe>
+                                     <h4><a href="#">VIDEO: At home in the world</a></h4>
+                                </li>
+                                <li>
+                                     <h5>
+                                        <img src="images/twitter32.png" alt="" />
+                                        <span>Twitter</span>
+                                        <a href="#">View more</a>
+                                     </h5>
+                                     <div class="tweet">
+                                     	<p>Tonight at #TheAUP "Ou va l'Amerique?", roundtable with incredible guests. RSVP now for last minutes spots <a href="#">http://t.co/JC16asP8</a> <a href="#">#aupevents</a></p>
+                                        <p>Oct 08, 2012</p>
+                                     </div>
+                                     <div class="tweet last">
+                                     	<p>Fantastic event tonight at #TheAUP, 19h00 "Ou va l'Amerique?" with <a href="#">@gbiassette</a> <a href="#">#ChristineOckrent</a> <a href="#">http://t.co/Q5VVILDI</a> - Open to the... </p>
+                                        <p>Oct 08, 2012</p>
+                                     </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- End of Content -->
+                    
+                    
+                    
+                    
+                    
+                    <!-- Start of footer -->
+                    <div class="footer">
+						<h6>&copy; 2013 THE AMERICAN UNIVERSITY OF PARIS</h6>
+                        <ul>
+                        	<li><a href="#">Maps &amp; Directories</a></li>
+                        	<li><a href="#">Site A to Z</a></li>
+                        	<li><a href="#">MyAUP</a></li>
+                        	<li class="last"><a href="#">Employment Opportunities</a></li>
+                        </ul>
+                        <ul class="icons">
+                        	<li><a href="#"><img src="images/facebook20.png" alt=""/></a></li>
+                        	<li><a href="#"><img src="images/twitter20.png" alt=""/></a></li>
+                        	<li><a href="#"><img src="images/youtube20.png" alt=""/></a></li>
+                        	<li><a href="#"><img src="images/rss20.png" alt=""/></a></li>
+                        </ul>
+                    </div>
+                    <!-- End of Footer -->
+                </div>
+                <!-- End of wrapper -->
+            </div>
+        </div>
